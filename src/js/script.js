255b66a905185cabd44812b8787262a97d1233e0
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-01-24 01:02:00 +0300

    added: third and fourth blocks

--- src/js/script.js
+++ src/js/script.js
@@ -2,57 +2,113 @@
 @@include("hover-effect.umd.js");
 @@include("gsap.min.js");
 
-// new hoverEffect({
-//     parent: document.querySelector(".distortion"),
-//     intensity: 0.2,
-//     image1: "../img/01.png",
-//     image2: "../img/02.png",
-//     displacementImage: "../img/diss.png",
-//     imagesRatio: 580 / 500
-// })
-
-let tl = gsap.timeline({paused: true});
-    tl.fromTo(".first", 1, {top: "-100vh"}, {duration: .7, opacity:1, top:0 });
-    // tl.fromTo(".second", 5, {top: "50",scale: 0}, {duration: .7, rotation: 360, top:0, scale: 1 });
-    // tl.fromTo(".third", 3, {top: "50", scale: 0}, {duration: .7, opacity: 1, top: 0, scale: 1 });
-    // tl.fromTo(".fourth", 2, {bottom: "-100vh", right: "-100vw"}, {duration: .7, opacity:1, bottom: 0, right: 0 });
-
-// const btn = document.querySelector(".btn")
-
-// btn.addEventListener("click",()=>{
-//     tl.reverse()
-// })
-
+// flags ----------------------------
 let activeScreen = 0
 
+// dom elements ---------------------------------------------------------
+const navigation = document.querySelector('.navigation')
 const navItems = document.querySelectorAll('.nav__item')
+
+const timeline = ()=> gsap.timeline({paused: true})
+
+// animations --------------------------------------------------------------------------
+const introAnim_1 = timeline().fromTo(".intro", 1.5, {left: "0"}, {duration: 10, left:"-100vw" });
+// const introAnim_2 = timeline().fromTo("#intro_text", 1.5, {marginLeft: 0}, {marginLeft: "0%"});
+const introAnim_3 = timeline().fromTo("#intro_img", 1, {marginLeft: 0}, {marginLeft: "-100%"});
+const introAnim_4 = timeline().fromTo("#intro_dots", 1.5, {right: 0}, {right: "-280px",top: "100%", position: "absolute", opacity: 0});
+
+const aboutAnim = timeline().fromTo(".about_complex", 1.5, {right: "-100vw",top:0}, {duration: 10, right:0 });
+// const aboutAnimClose = timeline().fromTo(".about_complex", 1.5, {duration: 10, right:0 }, {duration: 10, right:"110vw" });
+
+const roomsAnim = timeline().fromTo(".rooms", 1.5, {right: "-100vw"}, {duration: 10, right:"0" });
+
+// Menu items onclick animations --------------------------------------------------------------------
 
 navItems[0].addEventListener('click',(e)=>{
     e.preventDefault()
     if(activeScreen !== 1){
-        // let tl = gsap.timeline({delay:0});
-        gsap.fromTo(".intro", 1, {left: "0"}, {duration: .7, opacity:0, left:"-100vw" });
-        gsap.fromTo(".first", 1, {right: "-100vw",top:0}, {duration: .7, opacity:1, right:0 });
+        introAnim_1.play()
+        // introAnim_2.play()
+        introAnim_3.play()
+        introAnim_4.play()
 
-        // tl.play()
+        aboutAnim.play()
 
         activeScreen = 1
     }
     
 })
 
+navItems[1].addEventListener('click',(e)=>{
+    e.preventDefault()
+    if(activeScreen !== 2){
+        if(activeScreen == 0){
+            introAnim_1.play()
+            // introAnim_2.play()
+            introAnim_3.play()
+            introAnim_4.play()
+        }
+        navigation.classList.add('navigation-white')
+        roomsAnim.play()
+
+        activeScreen = 2
+    }
+    
+})
 
 
-// const secondBtn = document.querySelector(".second_btn")
+// Sliders ---------------------------------------------------------------
 
-// secondBtn.addEventListener("click",()=>{
-//     tl.play()
-// })
+const placesSlider = new Swiper('.placesSlider', {
+    loop:true,
+    speed: 1500,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
+  
+    navigation: {
+      nextEl: '#places-next',
+      prevEl: '#places-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
 
-// console.log(gsap)
+const aboutComplex = new Swiper('.aboutComplex', {
+    loop:true,
+    speed: 1500,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
-// let lastPos = 0
+    navigation: {
+      nextEl: '#about-next',
+      prevEl: '#about-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});
 
-// window.addEventListener('scroll',()=>{
+const standartComplex = new Swiper('.standartRoom', {
+    loop:true,
+    speed: 1500,
+  
+    pagination: {
+      el: '.swiper-pagination',
+    },
 
-// })
+    navigation: {
+      nextEl: '#standart-next',
+      prevEl: '#standart-prev',
+    },
+  
+    scrollbar: {
+      el: '.swiper-scrollbar',
+    },
+});standart
