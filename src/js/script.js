61efd5cca88f500c34158ac8933ce767066b4168
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-01-27 00:56:46 +0300

    adapted: 1024px

--- src/js/script.js
+++ src/js/script.js
@@ -462,3 +462,21 @@ const restaurantSlider = new Swiper('.restaurantSlider', {
     el: '.swiper-scrollbar',
   },
 });
+
+
+// MOBILE
+
+const navbarBtn = document.querySelector('#navbar__btn')
+const mobileBar = document.querySelector('.mobile_bar')
+
+navbarBtn.addEventListener('click', (e)=>{
+  navbarBtn.classList.toggle('burger_active')
+  mobileBar.classList.toggle('mb_bar_closed')
+  if(mobileBar.classList.contains('bot_0')){
+    mobileBar.classList.remove("bot_0")
+  }else{
+      setTimeout(()=>{
+          mobileBar.classList.add("bot_0")
+      },500)
+  } 
+})
