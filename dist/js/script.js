fc25b44bb7c4cd343e741822685a90e5f1b82f12
Arthur-Agadzhanyan <artur0505gold@gmail.com>
2022-01-28 23:25:01 +0300

    added: animations

--- dist/js/script.js
+++ dist/js/script.js
@@ -45,9 +45,6 @@ const navLogo = document.querySelector(".nav_logo")
 
 // animations --------------------------------------------------------------------------
 
-
-const aboutAnim = () => timeline().fromTo(".about_complex", 1.5, { right: "-100vw", top: 0, left: "initial", background: "white" }, { duration: 10, right: 0, display: 'block' }).play();
-
 const placesAnim_1 = () => timeline().fromTo("", 1,
   { transform: 'scale(0.4)' + ' translate(65%,35%)' },
   { duration: 1, transform: "scale(1)", transform: 'scale(1)' + 'translate(0px,0px)' }).play();
@@ -57,18 +54,9 @@ const placesAnim_2 = () => timeline().fromTo("#places-info", 1.5,
   { duration: 1, transform: 'translateY(0px)' }).play();
 
 
-
-const roomsAnim = () => timeline().fromTo(".rooms", 1.5, { right: "-100vw", left: 'initial' }, { duration: 10, right: "0vw", display: 'block' }).play();
-
-const restaurantAnim = () => timeline().fromTo(".restaurant", 1.5, { right: "-100vw", left: 'initial' }, { duration: 10, right: "0", display: 'block' }).play();
-
-const avocationsAnim = () => timeline().fromTo(".avocations", 1.5, { right: "-100vw", left: 'initial' }, { duration: 10, right: "0", display: 'flex' }).play();
-
-const mapAnim = () => timeline().fromTo(".map", 1.5, { right: "-100vw", left: 'initial' }, { duration: 10, right: "0", display: 'flex' }).play();
-
-const restaurantContentAnim = () => timeline().fromTo("#restaurant_content", 1.7, { transform: "translate(45%,0%)" }, { duration: 10, transform: "translate(0%,0%)" }).play();
-
 //------------------
+
+if(innerWidth > 1100){
 const cleanNavs = () => navItems.forEach((item) => {
   item.classList.remove("nav__item-active")
 })
@@ -78,7 +66,7 @@ let container = document.querySelector(".container")
 
 let sections = gsap.utils.toArray(".section");
 
-gsap.to(sections, {
+const scrollTween = gsap.to(sections, {
   xPercent: -100 * (sections.length - 1),
   ease: "none",
   scrollTrigger: {
@@ -90,28 +78,22 @@ gsap.to(sections, {
       inertia: false
     },
     // base vertical scrolling on how wide the container is so it feels more natural.
-    end: () => "+=" + (container.offsetWidth*6 - innerWidth),
-    markers: true
+    end: () => "+=" + (container.offsetWidth * 6 - innerWidth)
   }
 });
 
-
-// gsap.to(container, {
-//   x: () => -(container.offsetWidth - innerWidth) + "px",
-//   ease: "none",
-//   scrollTrigger: {
-//     trigger: container,
-//     invalidateOnRefresh: true,
-//     pin: true,
-//     scrub: 1.6,
-//     end: () => "+=" + (container.offsetWidth - innerWidth)
-//   }
-// })
-
+ScrollTrigger.create({
+  trigger: ".avocations",
+  containerAnimation: scrollTween,
+  toggleClass: "active",
+  start: "left center",
+  id: "3"
+});
 
 
 navLogo.addEventListener('click',function (event) {
   event.preventDefault();
+  cleanNavs()
   gsap.to("html", { scrollTo:{y:0}, duration: 1.5});
 })
 
@@ -129,81 +111,6 @@ navItems.forEach((navItem)=>{
     console.log(left)
 })
 })
-
-// navItems[0].addEventListener('click',function (event) {
-//     event.preventDefault();
-
-//     cleanNavs()
-//     event.currentTarget.classList.add("nav__item-active")
-
-//     let id = event.target.getAttribute('href')
-//     var left = document.querySelector(id).offsetLeft;
-//     gsap.to("html", { scrollTo: left, duration: 1.5});
-    
-//     console.log(left)
-// })
-
-// navItems[1].addEventListener('click',function (event) {
-//   event.preventDefault();
-
-//   cleanNavs()
-//   event.currentTarget.classList.add("nav__item-active")
-
-//   let id = event.target.getAttribute('href')
-//   var left = document.querySelector(id).offsetLeft;
-//   gsap.to("html", { scrollTo: 2200, duration: 1.5});
-  
-//   console.log(left)
-// })
-
-// navItems[2].addEventListener('click',function (event) {
-//   event.preventDefault();
-
-//   cleanNavs()
-//   event.currentTarget.classList.add("nav__item-active")
-
-//   let id = event.target.getAttribute('href')
-//   var left = document.querySelector(id).offsetLeft;
-//   gsap.to("html", { scrollTo:500, duration: 1.5});
-  
-//   console.log(left)
-// })
-
-// navItems[3].addEventListener('click',function (event) {
-//   event.preventDefault();
-
-//   cleanNavs()
-//   event.currentTarget.classList.add("nav__item-active")
-
-//   let id = event.target.getAttribute('href')
-//   var left = document.querySelector(id).offsetLeft;
-//   gsap.to("html", { scrollTo:500, duration: 1.5});
-  
-//   console.log(left)
-// })
-
-// navItems[4].addEventListener('click',function (event) {
-//   event.preventDefault();
-
-//   cleanNavs()
-//   event.currentTarget.classList.add("nav__item-active")
-
-//   let id = event.target.getAttribute('href')
-//   var left = document.querySelector(id).offsetLeft;
-//   gsap.to("html", { scrollTo:500, duration: 1.5});
-  
-//   console.log(left)
-// })
-
-
-// $(".menu-item").on("click", function (event) {
-//   event.preventDefault();
-//   var id = $(this).attr('href');
-//   var left = $(id)[0].offsetLeft;
-//   gsap.to("html", { scrollTo: left, duration: 1.5});
-// });
-
-// // scroll triggers----------------------------
 
 gsap.to('.text_bot', {
   scrollTrigger: {
@@ -261,309 +168,21 @@ gsap.to('.places_img_third', {
   transform: "translate(0%,5%)"
 })
 
-// const swipeIntro = ()=>{
-//   const introAnim_1 = timeline().fromTo(".intro", 1.5, {left: "0",right:"0"}, {duration: 10, left:"-100vw",display:'none' });
-//   // const introAnim_2 = timeline().fromTo("#intro_text", 1.5, {marginLeft: 0}, {marginLeft: "0%"});
-//   const introAnim_3 = timeline().fromTo("#intro_img", 1, {marginLeft: 0}, {marginLeft: "-100%"});
-//   const introAnim_4 = timeline().fromTo("#intro_dots", 1.5, {right: 0}, {right: "-280px",top: "100%", position: "absolute", opacity: 0});
 
-//   introAnim_1.play()
-//   introAnim_3.play()
-//   introAnim_4.play()
-// }
-
-// const swipeAbout = (bg='auto')=> timeline().fromTo(".about_complex", 2, {right: "0vw",left:"0vw"}, {duration: 10, right:"initial",left:"-100vw",background:bg}).play();
-
-// // close ---------------------------
-
-// const closeRooms = () => {
-//   timeline().to(".rooms", 1.5, {duration: 10,right:"-100vw",left:"initial",display:'none' }).play();
-// }
-
-// const closeRestaurant = ()=>{
-//   timeline().to(".restaurant", 1.5, {duration: 10,right:"-100vw",left:"initial",display:'none' }).play();
-// }
-
-// const closeAvocations = ()=>{
-//   timeline().to(".avocations", 1.5, {duration: 10,right:"-100vw",left:"initial",display:'none' }).play();
-// }
-
-// const closeMap = ()=>{
-//   timeline().to(".map", 1.5, {duration: 10,right:"-100vw",left:"initial",display:'none' }).play();
-// }
-
-// const closeBlock = (block)=>{
-//   timeline().to(block, 1.5, {duration: 10,right:"-100vw",left:"initial",display:'none' }).play();
-// }
-
-// // reverce -------
-// const reverceAbout = () => timeline().fromTo(".about_complex", 1.5, {left: "-100vw",top:0}, {duration: 10, left:0, background:'white',display:'block'  }).play();
-// const reverceRooms = () => timeline().fromTo(".rooms", 1.5, {left: "-100vw",top:0}, {duration: 10, left:0, background:'white',display:'block'  }).play();
-// const reverceAvocations = () => timeline().fromTo(".avocations", 1.5, {left: "-100vw",top:0}, {duration: 10, left: 0, background:'white',display:'flex'}).play();
-
-// const reverceBlock = (block) => {
-//   timeline().fromTo(block, 1.5, {left: "-100vw",top:0}, {duration: 10, left: 0, background:'white',display:'block'}).play();
-// }
-
-// // Menu items onclick animations --------------------------------------------------------------------
-
-// navLogo.addEventListener('click',(e)=>{
-//   if(activeScreen !== 0){
-//     cleanNavs()
-//     navigation.classList.remove('navigation-white')
-
-//     if(activeScreen == 1){ 
-//       closeBlock(".about_complex")
-//       reverceBlock('.intro')
-//     }
-//     if(activeScreen == 2){ 
-//       closeBlock(".rooms")
-//       reverceBlock('.intro')
-//     }
-//     if(activeScreen == 3){ 
-//       closeBlock(".restaurant")
-//       reverceBlock('.intro')
-//     }
-//     if(activeScreen == 4){
-//       closeBlock(".avocations")
-//       reverceBlock('.intro')
-//     }
-//     if(activeScreen == 5){
-//       closeBlock(".map")
-//       reverceBlock('.intro')
-//     }
-
-//     gsap.to("#intro_img", 1, {marginLeft: 0});
-//     gsap.to("#intro_dots", 1.5, {right: 0, top: 0, opacity: 1});
-
-//     activeScreen = 0
-//   }
-// })
-
-// navItems[0].addEventListener('click',(e)=>{
-//     e.preventDefault()
-//     if(activeScreen !== 1){
-//         cleanNavs()
-//         e.currentTarget.classList.add("nav__item-active")
-
-//         if(activeScreen == 0){
-//           swipeIntro()
-//         }
-
-//         aboutAnim()
-//         placesAnim_1()
-//         placesAnim_2()
-
-//         navigation.classList.remove('navigation-white')
-
-//         if(activeScreen == 2){ 
-//           closeRooms()
-//           reverceAbout()
-//         }
-
-//         if(activeScreen == 3){
-//           closeRestaurant()
-//           reverceAbout()
-//         }
-
-//         if(activeScreen == 4){
-//           closeAvocations()
-//           reverceAbout()
-//         }
-
-//         if(activeScreen == 5){
-//           closeMap()
-//           reverceAbout()
-//         }
-
-//         gsap.to('.text_bot',{
-//           scrollTrigger: {
-//             trigger: '.ab_cont',
-//             scroller: '.about_complex',
-//             start: 'center bottom '
-//           },
-//           duration: 1,
-//           transform: "translate(0%,0%)"
-//         })
-
-//         gsap.to('#aboutComplexSlider',{
-//           scrollTrigger: {
-//             trigger: '.ab_cont',
-//             scroller: '.about_complex',
-//             start: 'center bottom '
-//           },
-//           duration: 1,
-//           transform: "translate(0%,0%)"
-//         })
-
-//         gsap.to('.places_img',{
-//           scrollTrigger: {
-//             trigger: '.places_img',
-//             scroller: '.about_complex',
-//             start: 'bottom bottom ',
-//             end: 'bottom center',
-//             scrub: 1,
-//           },
-//           duration: 1,
-//           transform: "translate(0%,10%)"
-//         })
-
-//         gsap.to('.places_img_second',{
-//           scrollTrigger: {
-//             trigger: '.places_img',
-//             scroller: '.about_complex',
-//             start: 'bottom bottom ',
-//             end: 'bottom center',
-//             scrub: 1,
-//           },
-//           duration: 1,
-//           transform: "translate(0%,15%)"
-//         })
-
-//         gsap.to('.places_img_third',{
-//           scrollTrigger: {
-//             trigger: '.places_img',
-//             scroller: '.about_complex',
-//             start: 'bottom bottom ',
-//             end: 'bottom center',
-//             scrub: 1,
-//           },
-//           duration: 1,
-//           transform: "translate(0%,5%)"
-//         })
-
-//         activeScreen = 1
-//     }
-
-// })
-
-// navItems[1].addEventListener('click',(e)=>{
-//     e.preventDefault()
-
-//     if(activeScreen !== 2){
-//         cleanNavs()
-//         e.currentTarget.classList.add("nav__item-active")
-//         if(activeScreen == 0) {
-//           swipeIntro()
-//         }
-
-//         if(activeScreen == 1) swipeAbout("#306261")
-
-//         if(activeScreen == 3){
-//           closeRestaurant()
-//           reverceRooms()
-//         }
-
-//         if(activeScreen == 4){
-//           closeAvocations()
-//           reverceRooms()
-//         }
-
-//         if(activeScreen == 5){
-//           closeMap()
-//           reverceRooms()
-//         }
-
-//         roomsAnim()
-
-//         navigation.classList.add('navigation-white')
-
-
-//         activeScreen = 2
-//     }
-
-// })
-
-// navItems[2].addEventListener('click',(e)=>{
-//   e.preventDefault()
-//   if(activeScreen !== 3){
-//     cleanNavs()
-//     e.currentTarget.classList.add("nav__item-active")
-
-//       if(activeScreen == 0) swipeIntro()
-//       if(activeScreen == 1) swipeAbout()
-
-//       if(activeScreen == 2){
-//         timeline().fromTo(".rooms", 1.5, {right: "0vw",left:"0vw"}, {duration: 10, right:"initial",left:"-100vw"}).play();
-//       }
-
-//       if(activeScreen == 4){
-//         closeAvocations()
-//         reverceBlock('.restaurant');
-//       }
-
-//       if(activeScreen == 5){
-//         closeMap()
-//         reverceBlock('.restaurant');
-//       }
-
-//       restaurantAnim()
-//       restaurantContentAnim()
-//       navigation.classList.remove('navigation-white')
-
-//       activeScreen = 3
-//   }
-
-// })
-
-// navItems[3].addEventListener('click',(e)=>{
-//   e.preventDefault()
-//   if(activeScreen !== 4){
-//     cleanNavs()
-//     e.currentTarget.classList.add("nav__item-active")
-
-//       if(activeScreen == 0) swipeIntro()
-//       if(activeScreen == 1) swipeAbout()
-
-//       if(activeScreen == 2){
-//         timeline().fromTo(".rooms", 1.5, {right: "0vw",left:"0vw"}, {duration: 10, right:"initial",left:"-100vw"}).play();
-//       }
-
-//       if(activeScreen == 3){
-//         timeline().fromTo(".restaurant", 2, {right: "0vw",left:"0vw"}, {duration: 10, right:"initial",left:"-100vw"}).play();
-//       }
-
-//       if(activeScreen == 5){
-//         closeMap()
-//         reverceAvocations('.avocations');
-//       }
-
-//       navigation.classList.remove('navigation-white')
-//       avocationsAnim()
-//       timeline().fromTo(".avocation_block", 1.3, {height: "0%"}, {duration: 10, height: "auto"}).play();
-//       timeline().fromTo(".avocation_block_1s", 1, {height: "0%"}, {duration: 10, height: "auto"}).play();
-
-
-//       activeScreen = 4
-//   }
-// })
-
-// navItems[4].addEventListener('click',(e)=>{
-//   e.preventDefault()
-//   if(activeScreen !== 5){
-//     cleanNavs()
-//     e.currentTarget.classList.add("nav__item-active")
-
-//       if(activeScreen == 0) swipeIntro()
-//       if(activeScreen == 1) swipeAbout("#306261")
-
-//       if(activeScreen == 2){
-//         timeline().fromTo(".rooms", 1.5, {right: "0vw",left:"0vw"}, {duration: 10, right:"initial",left:"-100vw"}).play();
-//       }
-
-//       if(activeScreen == 4){
-//         timeline().fromTo(".avocations", 2, {right: "0vw",left:"0vw"}, {duration: 10, right:"initial",left:"-100vw"}).play();
-//         timeline().to(".avocation_block", 1.3,{height: "0%"}).play();
-//         timeline().to(".avocation_block_1s", 1.3, {height: "0%"}).play();
-//       }
-
-//       navigation.classList.add('navigation-white')
-//       mapAnim()
-
-//       activeScreen = 5
-//   }
-// })
-
+  window.addEventListener('scroll',()=>{
+    for (let i = 1; i < 6; i++) {
+      if(window.scrollY == innerWidth * i){
+        cleanNavs()
+        if(i==2 || i==5){
+          navigation.classList.add('navigation-white')
+        }else{
+          navigation.classList.remove('navigation-white')
+        }
+        navItems[i-1].classList.add("nav__item-active")
+      }
+    }
+  })
+}
 
 // Sliders ---------------------------------------------------------------
 
